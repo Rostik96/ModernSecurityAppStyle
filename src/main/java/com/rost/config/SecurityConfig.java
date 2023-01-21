74e7a209947a8f00df8d9c32c3ca84b3858fbaf9
Rost <coderost@yandex.ru>
2023-01-21 19:23:35 +0300

    Modify SecurityConfig: – disable CSRF; – set up auth; - set up logging in;

--- src/main/java/com/rost/config/SecurityConfig.java
+++ src/main/java/com/rost/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.rost.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,13 +15,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    //private final AuthenticationProvider authProviderImpl;
     private final PersonDetailsService personDetailsService;
 
+    /**
+     * Настраиваем саму логику аутентификации.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-        //builder.authenticationProvider(authProvider);
         builder.userDetailsService(personDetailsService);
+    }
+
+
+    /**
+     * Конфигурируем сам SpringSecurity.
+     * Конфигурируем авторизацию.
+     * Все правила в конфиге читаются сверху вниз от более специфичных к более общим.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                //Настраиваем авторизацию
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll() //Неавторизованным пользователям можно только сюда.
+                .anyRequest().authenticated()
+
+                //Настраиваем страничку входа.
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login") //Куда отправятся данные с формы?
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
     }
 
     @Bean
