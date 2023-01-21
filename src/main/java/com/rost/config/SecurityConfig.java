ea193e8dcdc61b71e11d0c756c113fc9cebed53b
Rost <coderost@yandex.ru>
2023-01-21 18:39:22 +0300

    Refactor SecurityConfig without custom AuthenticationProvider. Refactor main-class.

--- src/main/java/com/rost/config/SecurityConfig.java
+++ src/main/java/com/rost/config/SecurityConfig.java
@@ -1,19 +1,30 @@
 package com.rost.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.rost.services.PersonDetailsService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthenticationProvider authProvider;
+    //private final AuthenticationProvider authProviderImpl;
+    private final PersonDetailsService personDetailsService;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder builder) {
-        builder.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        //builder.authenticationProvider(authProvider);
+        builder.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
