a5f8dbe2d787fb657cef9483603e6f4d011b9a3e
Rost <coderost@yandex.ru>
2023-01-21 18:22:22 +0300

    Create entity wrapper for Security

---
+++ src/main/java/com/rost/security/PersonDetails.java
@@ -0,0 +1,50 @@
+package com.rost.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.rost.models.Person;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter //Необходимо для получения данных аутентифицированного пользователя.
+public class PersonDetails implements UserDetails {
+    private final Person principal;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return principal.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return principal.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; //stub
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; //stub
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; //stub
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; //stub
+    }
+}
