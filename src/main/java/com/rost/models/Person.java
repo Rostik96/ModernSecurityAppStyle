708b6e0cb5c4afcf0b2fd0ce63cc7ae5dcc8e5b3
Rost <coderost@yandex.ru>
2023-01-21 18:22:04 +0300

    Create entity

---
+++ src/main/java/com/rost/models/Person.java
@@ -0,0 +1,30 @@
+package com.rost.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String name;
+
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+}
