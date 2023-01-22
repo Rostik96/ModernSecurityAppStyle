b5bcee2d2801a653689654c142c4dbbd62a8b765
Rost <coderost@yandex.ru>
2023-01-22 12:05:42 +0300

    Replace getter on simple method.

--- src/main/java/com/rost/enums/Role.java
+++ src/main/java/com/rost/enums/Role.java
@@ -1,14 +1,16 @@
 package com.rost.enums;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Getter
 public enum Role {
     ROLE_GUEST("GUEST"),
     ROLE_USER("USER"),
     ROLE_ADMIN("ADMIN");
 
     private final String shortName;
+
+    public String shortName() {
+        return shortName;
+    }
 }
