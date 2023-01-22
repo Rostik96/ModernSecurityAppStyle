bc56202ccfecee13a6c61e4535acd8bf18c1d69d
Rost <coderost@yandex.ru>
2023-01-22 11:12:24 +0300

    Enable SCRF.

--- src/main/java/com/rost/config/SecurityConfig.java
+++ src/main/java/com/rost/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.rost.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.rost.services.PersonDetailsService;
@@ -34,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 //Настраиваем авторизацию
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/error", "/auth/registration").permitAll() //Неавторизованным пользователям можно только сюда.
