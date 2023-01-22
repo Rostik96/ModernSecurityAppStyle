8b61b4f8f7e719acdcef3ce7cea5acf87dd2725b
Rost <coderost@yandex.ru>
2023-01-22 10:48:25 +0300

    Add password encoder bean's functional in AuthController mechanic.

--- src/main/java/com/rost/controllers/AuthController.java
+++ src/main/java/com/rost/controllers/AuthController.java
@@ -1,14 +1,42 @@
 package com.rost.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rost.models.Person;
+import com.rost.services.RegistrationService;
+import com.rost.util.PersonValidator;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    private final RegistrationService registrationService;
+    private final PersonValidator personValidator;
+
     @GetMapping("/login")
     public String loginPage() {
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("person") Person person) {
+        return "/auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+        if (bindingResult.hasErrors())
+            return "/auth/registration";
+        registrationService.register(person);
+        return "redirect:/auth:/login"; //stub
     }
 }
